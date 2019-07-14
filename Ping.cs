3e0270f7a37fa8a9440f1b1115707f6e6709b2a5
Student <example@example.com>
2019-07-14 12:43:29 -0700

    inital commit

---
+++ Ping.cs
@@ -0,0 +1,20 @@
+using System;
+
+class ping
+{
+  static void Main()
+  {
+      String str = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15";
+    console.WriteLine("NewString : " + str);
+    
+
+    for (int str = 0; str <= 10; year ++)
+    {
+      if (str == 3)
+      {
+        string Replace("3", "ping");
+        console.WriteLine(str);
+      }
+    }
+  }
+}
